03d98eb7a7ab37e4e990309a58058620663949b4
ViktorGakis <92163319+ViktorGakis@users.noreply.github.com>
2023-10-04 16:58:27 +0300

    keymaps, custom plugins

---
+++ lua/custom/keymaps/keymaps.lua
@@ -0,0 +1,7 @@
+-- rest keymaps
+map('n', '<leader>o', 'o<ESC>')
+map('n', '<leader>O', 'O<ESC>')
+map('n', '<C-j>', '<CMD>move .+1<CR>')
+map('n', '<C-k>', '<CMD>move .-2<CR>')
+map('x', '<C-j>', ":move '>+1<CR>gv=gv")
+map('x', '<C-k>', ":move '<-2<CR>gv=gv")
